3bc9c44255d77e90f326cd9049ca91c794abb69f
Carlos Delgadillo <83809422+carlosd-02@users.noreply.github.com>
2026-02-24 23:00:38 -0800

    Enhanced error handling.

--- web/src/api/weather.ts
+++ web/src/api/weather.ts
@@ -16,6 +16,7 @@ export type WeatherParams = {
 }
 
 export async function fetchWeatherData(params: WeatherParams): Promise<WeatherDTO | { error: string }> {
+    try {
     const urlParams = new URLSearchParams({
         city: params.city,
         country: params.country,
@@ -28,17 +29,22 @@ export async function fetchWeatherData(params: WeatherParams): Promise<WeatherDT
     }
 
     console.log(`Constructed URL: /api/weather?${urlParams.toString()}`);
+    console.log(`Making API call to /api/weather with params:`, params);
     const response = await fetch(`/api/weather?${urlParams.toString()}`);
     console.log(`Received response with status: ${response.status}`);
     const data = await response.json();
     if (!response.ok) {
         console.error('Error response from API:', data);
-        return { error: data.message || 'Unknown error' };
+        return { error: data.error + '; If this is an API missing error. Consider checking your API key in your .env file. See README for more details.' || 'Unknown error' };
     }
 
     console.log('Parsed weather data:', data);
 
     return data as WeatherDTO;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return { error: 'Failed to fetch weather data or data is empty. Please check if server is running.' };
+    }
 }
 
 export function dataToWeatherDTO(data: any): WeatherDTO {
